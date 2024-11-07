09bd07c18e230e4f16eab316da02f1d68d4d94d5
Mateus Silva Branco <143047033+MateusBranco02@users.noreply.github.com>
2024-11-07 19:57:21 -0300

    Criando a estrutura do projeto

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import '../styles/Home.css';
+
+export default function Home() {
+
+  return (
+    <>
+      <div>
+        <h1>Teste</h1>
+      </div>
+    </>
+  );
+}
