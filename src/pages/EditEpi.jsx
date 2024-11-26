0b8e0a89838d9954194b6c759ec850983a590ea3
leandro-v-c <133062540+leandro-v-c@users.noreply.github.com>
2024-11-26 20:21:14 -0300

    Criado a requisição para exibir os Epis

--- src/pages/EditEpi.jsx
+++ src/pages/EditEpi.jsx
@@ -1,16 +1,46 @@
 import '../styles/EditEpi.css';
 import Header from '../components/Header.jsx';
 import Footer from '../components/Footer.jsx';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 export default function EditEpi() {
+  const [epis, setEpis] = useState([]);
+
+  useEffect(() => {
+    listarEpis();
+  }, []);
+
+  const listarEpis = async () => {
+    const url = 'http://localhost:3000/epis';
+    try {
+      const response = await axios.get(url);
+      console.log(response.data.resultado);
+      setEpis(response.data.resultado);
+    } catch (error) {
+      console.log('Erro ao buscar os EPIS!', error);
+    }
+  }
+
   return (
     <>
       <Header titulo={"Bonde da S.A."} />
+
       <div className="container">
         <div className="container-historico">
           <h2>Editar/Remover</h2>
           <div className="historico-area">
-            <p>Esta área será usada para exibir os EPis.</p>
+            <ul className='lista-epis'>
+              {epis.map((epi, key) =>
+                <li className='epi-item' key={key}>
+                  <img className='foto-epi' src={epi.imagem} alt={epi.nome} />
+                  <div className='container-info'>
+                    <p>Item: {epi.nome}</p>
+                    <p>Quantidade: {epi.quantidade}</p>
+                  </div>
+                </li>
+              )}
+            </ul>
           </div>
         </div>
       </div>
